c7cabf9b88a79a30d5497c3a6e39105b40914108
sdac-7 <sdac-7@sdac-7-PC>
2024-07-30 08:44:02 +0530

    MAIN3

--- demo1/src/git/Main.java
+++ demo1/src/git/Main.java
@@ -4,6 +4,7 @@ public class Main {
 public static void main(String[] args) {
 	System.out.println("main function 1");
 	System.out.println("main function 2");
+	System.out.println("maIN 3");
 	
 }
 }
